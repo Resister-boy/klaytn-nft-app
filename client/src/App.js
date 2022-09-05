3b3ccbf317d05f4c41a25b5d143c3385f7dc075a
zhy2on <dhwlgus5940@gmail.com>
2022-09-05 21:39:56 +0900

    Feat: login/register

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react';
+import Market from "./pages/market/Market";
+import Home from './pages/home/Home';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+function App() {
+  const user = false;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ user ? <Home /> : <Navigate to="/market" /> } />
+        <Route path="/market" element={<Market />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
