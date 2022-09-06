b4f93f67d1406cdc5bf793870791109f76c41fae
zhy2on <dhwlgus5940@gmail.com>
2022-09-06 18:05:31 +0900

    Fix: useEffect dependency array 변경

---
+++ client/src/components/myPosts/myPosts.jsx
@@ -0,0 +1,30 @@
+import Post from "../post/Post";
+import "./myPosts.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function MyPosts({ userId }) {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await axios.get("/posts/user/" + userId);
+      setPosts(
+        response.data.sort((p1, p2) => {
+          return new Date(p2.createdAt) - new Date(p1.createdAt);
+        })
+      );
+    }
+    fetchPosts();
+  }, []);
+
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        {posts.map((p) => (
+          <Post key={p._id} post={p} />
+        ))}
+      </div>
+    </div>
+  );
+}
