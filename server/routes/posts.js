d35d4ce9779da3964b798a19dd85ecae88b28b1a
zhy2on <dhwlgus5940@gmail.com>
2022-09-06 16:06:02 +0900

    Feat: server post api

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -55,6 +55,7 @@ router.get("/:id", async (req, res) => {
 //get timeline posts
 router.get("/timeline/all", async (req, res) => {
 	try {
+		// const allPosts = await Post.find().sort(-date);
 		const currentUser = await User.findById(req.body.userId);
 		const userPosts = await Post.find({ userId: currentUser._id });
 		const friendPosts = await Promise.all(
@@ -62,8 +63,10 @@ router.get("/timeline/all", async (req, res) => {
 				return Post.find({ userId: friendId });
 			})
 		);
-		res.json(userPosts.concat(...friendPosts))
+		res.status(200).json(allPosts);
 	} catch (err) {
 		res.status(500).json(err);
 	}
 });
+
+module.exports = router
